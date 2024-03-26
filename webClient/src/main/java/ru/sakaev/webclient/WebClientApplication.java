c1e32979fedadc1271d9547373d1ac153bfa72d6
FinalExamSpec <hardist.g@gmail.com>
2024-03-26 07:41:52 +0300

    Добавил настройки бд

--- webClient/src/main/java/ru/sakaev/webclient/WebClientApplication.java
+++ webClient/src/main/java/ru/sakaev/webclient/WebClientApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@Configuration
+@ComponentScan("ru.sakaev.webclient.repositories")
 public class WebClientApplication {
 
     public static void main(String[] args) {
--- webClient/src/main/java/ru/sakaev/webclient/repositories/CompetitionRepository.java
+++ webClient/src/main/java/ru/sakaev/webclient/repositories/CompetitionRepository.java
@@ -1,7 +1,10 @@
 package ru.sakaev.webclient.repositories;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.sakaev.webclient.entity.Competition;
 
+@Repository
 public interface CompetitionRepository extends ReactiveCrudRepository<Competition, String> {
+    // методы репозитория
 }
